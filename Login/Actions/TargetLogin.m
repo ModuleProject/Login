ae8145ef7f7457e1b99005a3b6ad234aa8951d76
YM <yangming@1yd.me>
2016-06-12 13:22:53 +0800

    de

--- Login/Actions/TargetLogin.m
+++ Login/Actions/TargetLogin.m
@@ -12,7 +12,7 @@
 @implementation TargetLogin
 
 - (UIViewController *)ActionFetchViewControllerLogin:(NSDictionary *)dic{
-    return  [[LoginVC alloc] init];
+    return  [[LoginVC alloc] initWithNibName:@"LoginVC" bundle:nil];
 }
 
 @end
